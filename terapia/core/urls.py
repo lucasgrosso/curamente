4026bf8b9361589f35bfec63aec5d92d61dad463
lucasgrossosouza <seuemail@exemplo.com>
2024-06-06 20:06:14 -0300

    Teste commit

--- terapia/core/urls.py
+++ terapia/core/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from .import views
+from . import views
+from django.urls import path, include
+from .views import TerapeutaPacientesView
+
+
+
+
 
 urlpatterns = [
-    path('home_core', views.home_core, name="home_core"),
+    # Profile
+    path("usuario/", views.UsuarioView.as_view(), name="terapeuta"),
+    path("usuario/<str:pk>/", views.UsuarioDetail.as_view(), name="paciente"),
+path('terapeuta/<int:pk>/pacientes/', TerapeutaPacientesView.as_view(), name='terapeuta-pacientes'),
+
+
 
 ]
